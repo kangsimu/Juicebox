660a3467c7ea0e10841cb52d5281c02eff22820b
PrestonS95 <prestonsullins95@gmail.com>
2022-07-15 12:14:06 -0500

    Fixed the createUser function in index.js

--- db/index.js
+++ db/index.js
@@ -6,16 +6,20 @@ const client = new Client('postgres://localhost:5432/juicebox-dev');
 
 async function createUser({ username, password }) {
   try {
-    const result = await client.query(`
-        INSERT INTO users(username, password) 
-        VALUES ($1, $2);`,
-        ["some_name", "some_password"]);
+    const result = await client.query(
+      `
+      INSERT INTO users(username, password)
+      VALUES ($1, $2);
+    `,
+      [username, password]
+    );
 
     return result;
   } catch (error) {
     throw error;
   }
 }
+
 
 async function getAllUsers() {
     const { rows } = await client.query(
