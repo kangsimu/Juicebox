deb6348e8d436826198f07e81e2bd6300f2da4a4
PrestonS95 <prestonsullins95@gmail.com>
2022-07-15 13:04:21 -0500

    finished updating seed.js, finished part one

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const client = new Client('postgres://localhost:5432/juicebox-dev');
 
 async function createUser({ username, password }) {
   try {
-    const result = await client.query(
+    const {rows} = await client.query(
       `
      INSERT INTO users(username, password) 
       VALUES($1, $2) 
@@ -14,7 +14,7 @@ async function createUser({ username, password }) {
       RETURNING *;
       `, [username, password]);
 
-    return result;
+    return rows;
   } catch (error) {
     throw error;
   }
--- db/seed.js
+++ db/seed.js
@@ -14,8 +14,16 @@ const {
         username: "albert",
         password: "bertie99",
       });
+      const sandra = await createUser({
+        username: "sandra",
+        password: "2sandy4me",
+      });
+      const glamgal = await createUser({
+        username: "glamgal",
+        password: "soglam",
+      });
 
-      console.log(albert);
+      console.log(albert,sandra,glamgal);
 
       console.log("Finished creating users!");
     } catch (error) {
