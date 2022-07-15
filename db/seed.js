d6486d4d74a85e0fdff21495477efd22584899a5
Simu <kangtaeam@gmail.com>
2022-07-15 12:01:53 -0400

    switching seats

--- db/seed.js
+++ db/seed.js
@@ -1,24 +1,71 @@
-// inside db/seed.js
+// db/seed.js
 
-// grab our client with destructuring from the export in index.js
-const { client } = require('./index');
-
-async function testDB() {
-  try {
-    // connect the client to the database, finally
-    client.connect();
-
-    // queries are promises, so we can await them
-    const result = await client.query(`SELECT * FROM users;`);
-
-    // for now, logging is a fine way to see what's up
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  } finally {
-    // it's important to close out the client connection
-    client.end();
+const {
+    client,
+    getAllUsers
+  } = require('./index');
+  
+  async function dropTables() {
+    try {
+      console.log("Starting to drop tables...");
+  
+      await client.query(`
+        DROP TABLE IF EXISTS users;
+      `);
+  
+      console.log("Finished dropping tables!");
+    } catch (error) {
+      console.error("Error dropping tables!");
+      throw error;
+    }
   }
-}
-
-testDB();
+  
+  async function createTables() {
+    try {
+      console.log("Starting to build tables...");
+  
+      await client.query(`
+        CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          username varchar(255) UNIQUE NOT NULL,
+          password varchar(255) NOT NULL
+        );
+      `);
+  
+      console.log("Finished building tables!");
+    } catch (error) {
+      console.error("Error building tables!");
+      throw error;
+    }
+  }
+  
+  async function rebuildDB() {
+    try {
+      client.connect();
+  
+      await dropTables();
+      await createTables();
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  async function testDB() {
+    try {
+      console.log("Starting to test database...");
+  
+      const users = await getAllUsers();
+      console.log("getAllUsers:", users);
+  
+      console.log("Finished database tests!");
+    } catch (error) {
+      console.error("Error testing database!");
+      throw error;
+    }
+  }
+  
+  
+  rebuildDB()
+    .then(testDB)
+    .catch(console.error)
+    .finally(() => client.end());
