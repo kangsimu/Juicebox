00021a62b14170f2bef0b46078b311d87ebafd03
PrestonS95 <prestonsullins95@gmail.com>
2022-07-20 08:57:51 -0500

    add api folder

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// inside index.js
 const PORT = 3000;
-const express = require('express');
+const express = require("express");
 const server = express();
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
 
 server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
+  console.log("The server is up on port", PORT);
 });
