7cf4f0567a62fe3b0d953d5ac942291f5f9461e4
PrestonS95 <prestonsullins95@gmail.com>
2022-07-21 14:33:13 -0500

    'Updated the tags.js to read url tags'

--- api/tags.js
+++ api/tags.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db')
+const { getAllTags ,getPostsByTagName } = require('../db')
 tagsRouter.use((req, res, next) => {
-  console.log("A request is being made to /users");
+  console.log("A request is being made to /tags");
 
   next();
 });
 
 tagsRouter.get('/', async (req, res) => {
     const tags = await getAllTags();
-  
     res.send({"tags": tags});
 });
 
 tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  
+  let tag  = req.params.tagName
+
+  console.log( tag)
   // read the tagname from the params
   try {
+   const tagPosts = await getPostsByTagName(tag)
+   console.log(tagPosts)
+   res.send({ posts: tagPosts })
     // use our method to get posts by tag name from the db
     // send out an object to the client { posts: // the posts }
   } catch ({ name, message }) {
+    next ({name, message})
     // forward the name and message to the error handler
   }
 });
